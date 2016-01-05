f0a0a4b4b612804e830bfc67a0392152b4f7fda2
GregoryW <email": null>
2016-01-04 20:06:23 -0800

    tabbed interface jquery drill

--- tabs.js
+++ tabs.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
   // Put your JavaScript behavior here
+
+  $( '.tabs li' ).on('click', function(){
+    $( '.active' ).removeClass( 'active' );
+    $( this ).addClass( 'active' );
+
+    var divId = $( this.firstChild ).attr('href');
+    var $newText = $( divId ).html();
+    var $existingText = $( '.tab-content' );
+
+    $( '.tab-content' ).hide();
+    $( divId ).show(  );
+  });
+
 });
