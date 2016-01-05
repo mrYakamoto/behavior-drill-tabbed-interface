15370b84d1da94abcfab3b43f43fe133463233b1
Shawn Spears & Walter Kerr <pair+shawnspears.walterkerr@devbootcamp.com>
2016-01-04 17:01:06 -0800

    Add tabbing functionality

--- tabs.js
+++ tabs.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
-  // Put your JavaScript behavior here
+$(document).ready (function() {
+  $('.tab-content#integrity').show();
 });
+
+
+ $(document).ready(function () {
+   $(".tabs a").on ('click', function(e){
+     e.preventDefault();
+     var currentValue = $(this).attr('href');
+     $('.tab-content').hide();
+     $('.tabs li').removeClass('active');
+     $(currentValue).show();
+     $(this).parent().addClass('active');
+   });
+ });
